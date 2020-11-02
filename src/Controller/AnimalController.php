cdf5dadee4ce60c14c49988d5ade41553b1d4475
b3b3r <b3b3rseg@gmail.com>
2020-11-02 12:16:19 +0100

    display animal detail

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -11,13 +11,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AnimalController extends AbstractController
 {
     /**
-     * @Route("/", name="animal")
+     * @Route("/animals", name="animals-list")
      */
     public function index(AnimalRepository $repository): Response
     {
         $animals = $repository->findAll();
         return $this->render('animal/index.html.twig', [
             "animals" => $animals
+        ]);
+    }
+
+    /**
+     * @Route("/animals/{id}", name="animal-detail")
+     */
+    public function animalDetail(Animal $animal): Response
+    {
+        return $this->render('animal/detail.html.twig', [
+            "animal" => $animal
         ]);
     }
 }
--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -13,31 +13,41 @@ class AnimalFixtures extends Fixture
         $a1 = new Animal();
         $a1-> setName("Chien")
             ->setDescription("Un chien")
-            ->setPicture("chien.png");
+            ->setPicture("chien.png")
+            ->setWeight(10)
+            ->setIsDangerous(false);
         $manager->persist($a1);
 
         $a2 = new Animal();
         $a2-> setName("Serpent")
             ->setDescription("Un serpent")
-            ->setPicture("serpent.png");
+            ->setPicture("serpent.png")
+            ->setWeight(3)
+            ->setIsDangerous(true);
         $manager->persist($a2);
 
         $a3 = new Animal();
         $a3-> setName("Crocodile")
             ->setDescription("Un crocodile")
-            ->setPicture("crocodile.png");
+            ->setPicture("crocodile.png")
+            ->setWeight(150)
+            ->setIsDangerous(true);
         $manager->persist($a3);
 
         $a4 = new Animal();
         $a4-> setName("Requin")
             ->setDescription("Un requin")
-            ->setPicture("requin.png");
+            ->setPicture("requin.png")
+            ->setWeight(500)
+            ->setIsDangerous(true);
         $manager->persist($a4);
 
         $a5 = new Animal();
         $a5-> setName("Cochon")
             ->setDescription("Un cochon")
-            ->setPicture("cochon.png");
+            ->setPicture("cochon.png")
+            ->setWeight(250)
+            ->setIsDangerous(false);
         $manager->persist($a5);
 
         $manager->flush();
